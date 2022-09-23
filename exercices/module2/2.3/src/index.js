cba78a36793015c6a4ee7d3ccc07ccc796ca9315
juliendec <juliendecoen2308@gmail.com>
2022-09-23 10:30:58 +0200

    module2 2.3

---
+++ exercices/module2/2.3/src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+const lines = document.querySelector('#lines');
+const columns = document.querySelector('#columns');
+const startString = document.querySelector('#startString');
+const btn1 =  document.querySelector('#btn1')
+
+btn1.addEventListener('click', getFormulaire);
+
+function getFormulaire(){
+    
+}
